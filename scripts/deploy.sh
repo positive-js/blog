afbb231b3c97c5a0894fd56a617cac3ff247d2f8
Oleg Pimenov <opimenov@ptsecurity.com>
2017-08-28 00:19:45 +0300

    feat(post): css-font-metrics-line-height-and-vertical-align

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,10 @@ git clone https://$GITHUB_REPO
 cd $(basename ${GITHUB_REPO%.git})
 git config user.name "Travis CI"
 git config user.email ${GH_EMAIL}
+
 rsync -az --delete --exclude '.git*' ../_site/ .
 touch .nojekyll
-git add -A .
+
+git add --all .
 git commit -m "Generated Jekyll site by Travis CI - ${TRAVIS_BUILD_NUMBER}"
-git push --force "https://${GH_TRAVIS_TOKEN}@${GITHUB_REPO}" HEAD:${REPO_TARGET_BRANCH}
+git push -f "https://${GH_TOKEN}@${GITHUB_REPO}" HEAD:${REPO_TARGET_BRANCH}
